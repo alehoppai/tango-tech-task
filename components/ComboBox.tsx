6ea08618b8eccc08a025286dcfd4acab3d76e490
Aleh Belski <aleh_belski@outlook.com>
2024-09-16 15:47:26 +0200

    feat(TNG-7): Finalizing

--- components/ComboBox.tsx
+++ components/ComboBox.tsx
@@ -5,6 +5,7 @@ import { Joke, useJokes } from "@/hooks/useJokes";
 import { TextInput } from "./TextInput";
 import { List } from "./List";
 import { Popup } from "./Popup";
+import { InputIndicator } from "./InputIndicator";
 
 export const ComboBox = () => {
   const {
@@ -23,32 +24,40 @@ export const ComboBox = () => {
       setSelectedJoke(joke);
       setPopupOpen(false);
     },
-    [setSelectedJoke, setPopupOpen]
+    [setSelectedJoke]
   );
 
   return (
-    <div className="w-96 relative">
-      <TextInput
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        placeholder="Search for jokes"
-        rightSlot={isLoading ?? <div>Loading...</div>}
-        errorMessage={error ?? ""}
-        onFocus={() => setPopupOpen(true)}
-        onKeyDown={(e) => {
-          if (e.key === "Escape") {
-            setPopupOpen(false);
-            e.currentTarget.blur();
+    <div className="w-full flex-col items-center gap-2">
+      {selectedJoke && <h2>{selectedJoke.joke}</h2>}
+      <div className="w-96 relative">
+        <TextInput
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder="Search for jokes"
+          rightSlot={
+            <InputIndicator
+              isPopupOpen={popupOpen}
+              isResultsLoading={isLoading}
+            />
           }
-        }}
-      />
-      <Popup open={popupOpen} onClose={() => setPopupOpen(false)}>
-        <List
-          jokes={jokes}
-          selectedJoke={selectedJoke}
-          setSelectedJoke={onClickJoke}
+          errorMessage={error ?? ""}
+          onFocus={() => setPopupOpen(true)}
+          onKeyDown={(e) => {
+            if (e.key === "Escape") {
+              setPopupOpen(false);
+              e.currentTarget.blur();
+            }
+          }}
         />
-      </Popup>
+        <Popup open={popupOpen} onClose={() => setPopupOpen(false)}>
+          <List
+            jokes={jokes}
+            selectedJoke={selectedJoke}
+            setSelectedJoke={onClickJoke}
+          />
+        </Popup>
+      </div>
     </div>
   );
 };
